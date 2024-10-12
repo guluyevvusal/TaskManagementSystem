47d9a15dfa96a6a93a4f5501dc6462c2679005fe
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-10-12 16:50:02 +0400

    All created

---
+++ Core/TaskManagementSystem.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementSystem.Domain.Entities;
+
+namespace TaskManagementSystem.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ İnfrastructure/TaskManagementSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskManagementSystem.Domain.Entities;
+
+
+
+namespace TaskManagementSystem.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Entities.Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
