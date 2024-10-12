7e5a737126a766564a7bb7e102a27edc0c9973ee
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-10-12 17:09:23 +0400

    Ready

---
+++ PresentationLayer/TaskManagementSystem.Web/Controllers/AuthControllers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Application.DTOs;
+using TaskManagementSystem.Application.Interfaces;
+using TaskManagementSystem.Domain.Entities;
+
+namespace TaskManagementSystem.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthControllers : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthControllers(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserDTO userDto)
+        {
+            // İstifadəçi doğrulamasını burada həyata keçirin
+            // Əgər uğurludursa:
+            var token = _authService.GenerateToken(new User { Username = userDto.Username });
+            return Ok(new { Token = token });
+        }
+    }
+}
